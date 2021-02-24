3190c30e367e8f90d694267e034baecdc7be683b
mpaz <mpazaryna@gmail.com>
2021-02-24 15:16:32 -0500

    initial commit react-query

---
+++ explore-react-query/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Planets from "./components/Planets";
+import People from "./components/People";
+
+function App() {
+  let [page, setPage] = useState("planets");
+
+  return (
+    <>
+      <div className="App">
+        <h1>Star Wars API</h1>
+        <Navbar setPage={setPage} />
+        <div className="content">
+          {page === "planets" ? <Planets /> : <People />}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
